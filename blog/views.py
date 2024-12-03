8920c9a9db7e667c553bedd58d385d35df48b964
osm3rr <osm3rr@gmail.com>
2024-12-03 18:06:07 -0400

    publication list done

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Publication
 # Create your views here.
+
+class HomePageView(ListView):
+    model = Publication
+    template_name = "publication_list.html"
+
+
