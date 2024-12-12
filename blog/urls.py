c9c48c31b50b5d6a220bf958106501df170fb7c5
osm3rr <osm3rr@adakademy.com>
2024-12-12 17:28:42 -0400

    publication delete done

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from .views import HomePageView, PublicationCreateView, PublicationDetailView, PublicationUpdateView
+from .views import ( 
+                    HomePageView,
+                    PublicationCreateView,
+                    PublicationDetailView,
+                    PublicationUpdateView,
+                    PublicationDeleteView,
+                )
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path("publication/new/", PublicationCreateView.as_view(), name="publication_new"),
     path("publication/<int:pk>/", PublicationDetailView.as_view(), name="publication_detail"),
     path("publication/<int:pk>/update/", PublicationUpdateView.as_view(), name="publication_update"),
+    path("publication/<int:pk>/delete/", PublicationDeleteView.as_view(), name="publication_delete"),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Publication
+from django.urls import reverse_lazy
 # Create your views here.
 
 class HomePageView(ListView):
@@ -22,3 +23,8 @@ class PublicationUpdateView(UpdateView):
     model = Publication
     template_name = "publication_update.html"
     fields = ["title", "body"]
+
+class PublicationDeleteView(DeleteView):
+    model = Publication
+    template_name = "publication_delete.html"
+    success_url = reverse_lazy("home")
